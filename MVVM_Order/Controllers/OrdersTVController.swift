bf630b91561fa500e05aa3cbb1f46bccf00b4cf3
c0407h <67133244+c0407h@users.noreply.github.com>
2024-01-23 22:24:51 +0900

    api 호출 후 주문목록 TVCell에 데이터 바인딩

--- MVVM_Order/Controllers/OrdersTVController.swift
+++ MVVM_Order/Controllers/OrdersTVController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class OrdersTVController: UITableViewController {
 
+    var orderListViewModel = OrderListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,14 +25,34 @@ class OrdersTVController: UITableViewController {
         
         let resource = Resource<[Order]>(url: coffeeOrdersURL)
         
-        OrderService().load(resource: resource) { result in
+        OrderService().load(resource: resource) {[weak self] result in
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.orderListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.orderListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vm = self.orderListViewModel.orderViewModel(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTVCell", for: indexPath)
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        
+        return cell
     }
 }
 
